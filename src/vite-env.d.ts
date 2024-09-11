4fd61e32f3be3fe821e07749e2ec8d1e37d0c47c
duanluan <duanluan@outlook.com>
2024-09-11 13:48:44 +0800

    feat: 新增不同环境变量的使用

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  // 环境变量
+  readonly VITE_BASE_URL: string
+}
+
+interface ImportMeta {
+  // 环境变量
+  readonly env: ImportMetaEnv
+}
