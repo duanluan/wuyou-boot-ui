6d14fb4ca6bfc8591fc1d7f127a1cac808e29626
duanluan <duanluan@outlook.com>
2024-10-30 14:59:27 +0800

    feat: Element Plus 国际化为 zh-cn

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <router-view/>
+  <el-config-provider :locale="zhCn">
+    <router-view/>
+  </el-config-provider>
 </template>
 
 <script setup lang="ts">
+import {ElConfigProvider} from 'element-plus'
+// 国际化 | Element Plus：https://element-plus.org/zh-CN/guide/i18n.html#configprovider
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 </script>
